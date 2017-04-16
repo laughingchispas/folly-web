492d8a6334aa131ca2862962c5a47f7b6c667a05
Nicole Miller <laughingchispas@gmail.com>
2017-04-16 15:01:30 -0700

    add site footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SiteFooterComponent } from './component/site-footer/site-footer.cmp';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteFooterComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule
   ],
   providers: [],
