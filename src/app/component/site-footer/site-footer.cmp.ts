866597553f97528110dbeb429b375b8830ff9493
Nicole Miller <laughingchispas@gmail.com>
2017-04-16 15:02:46 -0700

    Add site footer for realz

---
+++ src/app/component/site-footer/site-footer.cmp.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component ({
+  selector: 'site-footer',
+  templateUrl: './site-footer.cmp.html',
+  styleUrls: ['./site-footer.cmp.css']
+})
+
+export class SiteFooterComponent {
+  email: string = 'laughingchispas@gmail.com';
+}
