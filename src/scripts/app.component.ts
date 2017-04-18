b37ca59cd22bd543234e93c4fc3c8b527d106832
Nicole Miller <laughingchispas@gmail.com>
2017-04-18 16:34:20 -0700

    add front slide-show

---
+++ src/scripts/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <div class="app">
+      Hello world!
+      <main>
+        <!-- stuff -->
+      </main>
+    </div>
+  `
+})
+export class AppComponent {
+
+}
